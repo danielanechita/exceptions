3cf51cb695d8000c2ae2be936a61718085277699
danielanechita <daniela.nechita85@gmail.com>
2018-12-15 02:16:54 -0800

    added custom exceptions

---
+++ ProiectNou/ProiectNou/DressCodeException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProiectNou
+{
+	class DressCodeException : ApplicationException
+	{
+
+		private readonly string message = " You are not wearing an Elf costume, and your are underage!";
+
+		public DressCodeException()
+		{
+			Console.WriteLine(message);
+		}
+
+
+
+
+
+	}
+}
--- ProiectNou/ProiectNou/Program.cs
+++ ProiectNou/ProiectNou/Program.cs
@@ -3,10 +3,53 @@
 namespace ProiectNou
 {
 	class Program
-	{
+	{  
+		
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			/*
+			try
+			{
+				int b = 100;
+				int a = b / 0;
+				Console.WriteLine(a);
+
+			}
+
+			catch(DivideByZeroException exception)
+			{
+				Console.WriteLine("Ups, something is wrong");
+				Console.WriteLine(exception.Message);
+				Console.WriteLine(exception.StackTrace);
+				
+			}
+			
+	          */
+			try
+			{
+				Person person = new Person(15);
+				Person person1 = new Person(-1);
+				
+				Person person3 = new Person(81);
+
+
+			}
+			catch (ArgumentException e)
+			{
+				Person person2 = new Person(2);
+				//Console.WriteLine(e)
+				Console.WriteLine(e.Message + e.GetType());
+
+				//throw;
+			}
+			catch(DressCodeException ex)
+			{
+				Console.WriteLine(ex.Message + ex.GetType());
+			}
+			
+
+
 		}
 	}
 }
